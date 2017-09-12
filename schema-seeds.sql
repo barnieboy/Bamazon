fbb25674d754914fd5cb34fd88bd8213546ba874
Tadesse Tegena <tadessetegena@Tadesses-MacBook-Pro.local>
2017-09-12 17:07:12 -0400

    adding

---
+++ schema-seeds.sql
@@ -0,0 +1,62 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hortilux 1000w HPS Lamp", "Lighting", 109.95, 56);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("LEC 4200K 315w System", "Lighting", 599.95, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sun Blaze 4ft 8 Lamp T5 Fixture", "Lighting", 267.95, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Botanicare CocoGro 50L", "Grow Media", 22.95, 127);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Perlite 4 cu ft", "Grow Media", 17.95, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hydroton Clay Pepples 50L", "Grow Media", 32.95, 82);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Emily\'s Garden", "Hydroponic Systems", 99.95, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Flo n\' Grow 12 Pot", "Hydroponic Systems", 624.95, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Botanicare CNS17 Bloom Gallon", "Nutrients", 49.95, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bloombastic 325ml", "Nutrients", 94.95, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Oakton EcoTestr pH Meter", "Meters", 74.95, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("BlueLab Truncheon TDS Meter", "Meters", 134.95, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hurricane 6 inch Inline Fan", "Fans", 119.95, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("16 inch Wall Mount Fan", "Fans", 36.95, 2);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Lighting", 4129.60, 0);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Grow Media", 5742.60, 0);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Hydroponic Systems", 3324.65, 0);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Nutrients", 629.55, 0);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Pumps", 611.88, 0);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Meters", 279.83, 0);
+
+INSERT INTO departments (department_name, over_head_costs, total_sales)
+VALUES ("Fans", 523.32, 0);
